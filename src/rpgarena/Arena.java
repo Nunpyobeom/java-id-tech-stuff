ff6e82bcad19e7b54032a732d9a031ef5445c38b
nunpyobeom <noahpower7@gmail.com>
2019-08-06 16:44:32 -0400

    even more stuff

--- src/rpgarena/Arena.java
+++ src/rpgarena/Arena.java
@@ -1,14 +1,27 @@
 package rpgarena;
 public class Arena {
-    public static void main(String[] args) {
 
-    Character player1 = new Character();
-    Character player2 = new Character();
+    public static void main(String[] args){
+        Character player1 = new Character();
+        Character player2 = new Character();
 
-    player2.name = "Steve";
-    player2.health = 50;
-        System.out.println(player2.name);
+        player1.name = "Billy Bob";
+        player1.strength = 2;
+        player1.health = 50;
+        player1.defense = 1;
+        player2.name = "Steve";
+        player2.strength = 1;
+        player2.health = 50;
+        player2.defense = 2;
+
         System.out.println(player1.name + " vs. " + player2.name);
-        System.out.println(player1.health + " vs. " + player2.health);
+        System.out.println(player1.health + " vs. "  + player2.health);
+        while(player1.isAlive() && player2.isAlive()){
+            System.out.println(player1.name + ": " + player1.health);
+            System.out.println(player2.name + " has: " + player2.health);
+            player1.attack(player2);
+            player2.attack(player1);
+        }
     }
+
 }
--- src/rpgarena/Character.java
+++ src/rpgarena/Character.java
@@ -1,8 +1,25 @@
 package rpgarena;
-
 public class Character {
+
         public String name;
         public int strength;
         public int health;
         public int defense;
+
+        public int takeDamage( int damage ){
+                int damageTaken = damage - this.defense;
+                this.health -= damageTaken;
+                return damageTaken;
+        }
+
+        public int attack(Character target){
+                int damage = this.strength * 2;
+                int damageDealt = target.takeDamage(damage);
+                return damageDealt;
+        }
+
+        public boolean isAlive(){
+                return this.health > 0;
+        }
+
 }
