0288a6aa9ed4c274e156011fd955d7e1d288fe41
nunpyobeom <noahpower7@gmail.com>
2019-08-08 07:48:10 -0700

    YAY more stuffs

--- src/PLAINDROME/Stacks.java
+++ src/PLAINDROME/Stacks.java
@@ -1,14 +1,20 @@
+package PLAINDROME;
+
 import java.util.*;
+import java.util.Scanner;
 public class Stacks {
 
     public static void main(String[] args) {
-    String WordtoCheck = "tacocat";
+    Scanner koolthing =  new Scanner(System.in);
+        System.out.println("Please enter your word:");
+
+    String WordtoCheck = koolthing.nextLine();
     Stack word = new Stack();
     String reverseWord = "";
     for (int i = 0; i < WordtoCheck.length(); i++){
         word.push(WordtoCheck.charAt(i));
     }
-    while (word.isEmpty() == false) {
+    while (!word.isEmpty()) {
         reverseWord = reverseWord + word.pop();
     }
     if (WordtoCheck.equals(reverseWord)) {
